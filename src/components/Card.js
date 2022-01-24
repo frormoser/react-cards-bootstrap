d01bef69ba1ee33de5ffcf5b2891e4405664365f
Federico <joelrormoser@gmail.com>
2022-01-24 09:29:15 -0300

    change background and style cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import img1 from '../assets/img1.jpeg'
 
 function Card() {
   return (
-    <div className="card">
-      <img src= {img1} alt="" width="400" height="340" />   
-      <div className='card-body'>
+    <div className="card text-center bg-dark">
+      <img src="" alt=""  />   
+      <div className='card-body text-light'>
         <h4 className='card-title'>My title</h4>
         <p className="card-text text-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in</p>
+        <a href='#!' className='btn btn-outline-secondary rounded-0'>Go to this website</a>
       </div>
     </div>
     )
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,19 +1,38 @@
 import React from 'react';
-import Card from './Card'
+import Card from './Card';
+import img1 from '../assets/img1.jpeg';
+import img2 from '../assets/img2.jpeg';
+import img3 from '../assets/img3.jpeg';
+
+const cards = [
+  {
+    id: 1,
+    title: 'Cards Web',
+    image: img1
+  },
+  {
+    id: 2,
+    title: 'Cards Blog',
+    image: img2
+  },
+  {
+    id: 3,
+    title: 'Cards Github',
+    image: img3
+  }
+];
 
 function Cards() {
   return (
   <div className="container d-flex justify-content-center align-items-center h-100">
     <div className='row'>
-      <div className='col-md-4'>
-        <Card/>       
-      </div>
-      <div className='col-md-4'>
-        <Card/>       
-      </div>
-      <div className='col-md-4'>
-        <Card/>       
-      </div>
+      {
+        cards.map(card => (
+        <div className='col-md-4' key={card.id}>
+          <Card/>    
+        </div> 
+          ))
+      }     
     </div>
   </div>
   )
